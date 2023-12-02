46cdf82a1fd40db3fee5f495ee56324b1eb2372a
Ариуна Уваннай <ariuna.uvannay@mail.ru>
2023-12-02 12:09:46 +0300

    part1

--- js/script.js
+++ js/script.js
@@ -4,6 +4,45 @@ alert (a);
 let year = 2007;
 alert (year);
 
-let name = "Брендан Эйх";
-alert (name);
+let names = "Брендан Эйх";
+alert (names);
 
+let ten = 10;
+let two = 2;
+let summ = ten + two;
+let raz = ten - two;
+let pro = ten * two;
+let chas = ten / two;
+alert (summ + raz + pro + chas); 
+
+let result = 2**5;
+alert (result);
+
+let a1 = 9;
+let b = 2;
+let cel = a1 % b;
+alert (cel);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+const user ={
+    name: 'Sony',
+    age: 29,
+    isAdmin: false,
+};
+user["city of Residence"] = 'Novosibirsk';
+
+
+//*9.1*//
+
+alert(user["city of Residence"])
