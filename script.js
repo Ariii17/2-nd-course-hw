44a0ecec054fbcff623ac8f417184856df3e678b
Ariii17 <150605060+Ariii17@users.noreply.github.com>
2025-02-01 23:58:00 +0700

    третье задание

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ alert(ask === password ? `Пароль введен верно` : `Пароль 
 
 let c = Number(prompt(`Введите число`));
 alert(c > 0 && c < 10 ? `Верно` : `Неверно`);
+
+let d = Number(prompt(`Введите число`));
+let e = Number(prompt(`Введите число`));
+alert(d > 100 || e > 100 ? `Верно` : `Неверно`);
+
