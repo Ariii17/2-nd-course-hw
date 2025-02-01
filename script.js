d3ccec420d8bbaa945c09312be056881a8241564
Ariii17 <150605060+Ariii17@users.noreply.github.com>
2025-02-02 00:24:40 +0700

    пятое задание

--- script.js
+++ script.js
@@ -15,3 +15,46 @@ let a = '2';
 let b = '3';
 // Код выше изменять менять нельзя. Чтобы решить задачу исправьте код ниже:
 alert(+a + +b);
+
+let monthNumber = Number(prompt(`Введите номер месяца`)); 
+
+switch (monthNumber) {
+    case (+12):
+        alert(`зима`);
+        break;
+    case (+1):
+        alert(`зима`);
+        break;
+    case (+2):
+        alert(`зима`);
+        break;
+    case (+3):
+        alert(`весна`);
+        break;
+    case (+4):
+        alert(`весна`);
+        break;
+    case (+5):
+        alert(`весна`);
+        break;
+    case (+6):
+        alert(`лето`);
+        break;
+    case (+7):
+        alert(`лето`);
+        break;
+    case (+8):
+       alert(`лето`);
+        break;
+    case (+9):
+        alert(`осень`);
+        break;
+    case (+10):
+        alert(`осень`);
+        break;
+    case (+11):
+        alert(`осень`);
+        break; 
+    default: alert(`Такого месяца нет`);
+    break;
+}
