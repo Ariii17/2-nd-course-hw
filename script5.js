ded4bb4ae69353cc03d641dc0e14cbf6baae33dd
Ariii17 <150605060+Ariii17@users.noreply.github.com>
2025-04-14 15:39:54 +0300

    4  task

--- script5.js
+++ script5.js
@@ -16,3 +16,12 @@ while (b <= 22) {
   console.log(b);
   b++;
 }
+const obj = {
+  "Коля": '200',
+  "Вася": '300',
+  "Петя": '400'
+};
+
+for (let key in obj) {
+  console.log(`${key} — ${obj[key]} долларов`);
+}
