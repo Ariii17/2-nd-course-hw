c4465e584a4973dcf3f3a191a6292d8cf94c723c
Ariii17 <ariuna.uvannay@mail.ru>
2025-04-18 17:52:20 +0300

    выполнила задания, не поняла как делать последнее задание

--- script6.js
+++ script6.js
@@ -5,7 +5,71 @@ function numberLess(a, b) {
       return b;
     }
   }
+
+  function chet(a) {
+    if (a % 2 == 0) {
+    alert("Число четное");
+    } else {
+    alert("Число нечетное");
+    }
+    }
+    
+    let age = parseInt(prompt("Пожалуйста, введите ваш возраст"));
+
+    if (isNaN(age)) {
+        alert("Пожалуйста, введите числовое значение для возраста.");
+    } else {
+      function userAge() {
+        if (age < 0) {
+          alert("Вы ввели неправильное значение (возраст не может быть отрицательным)");
+        } else if (age >= 13) {
+          alert("Добро пожаловать!");
+        } else if (age <= 12) {
+          alert("Привет, друг!");
+        }
+      }
+      userAge();
+    }
   
-  numberLess(8, 4);
-  
-  
+    function multNumb(a, b) {
+      if (a === null || a === undefined || b === null || b === undefined) {
+        alert("Одно или оба значения являются null или undefined");
+      }
+    
+      const numA = Number(a);
+      const numB = Number(b);
+    
+      if (isNaN(numA) || isNaN(numB)) {
+        alert("Одно или оба значения не являются числом");
+      } else {
+        alert(numA * numB);
+      }
+    }  
+    
+function outNumbSq(numC) {
+  const square = numC * numC;
+  console.log(square);
+}
+
+function getSq(numD) {
+  const square = numD * numD;
+  console.log(square);
+  return numD;
+}
+
+function countCube() {
+  const userInput = prompt("Пожалуйста, введите число:");
+
+  if (userInput === null) {
+    return "Ввод отменен пользователем.";
+  }
+
+  const numE = Number(userInput);
+
+  if (isNaN(numE)) {
+    alert('Переданный параметр не является числом');
+  } else {
+    const cube = numE ** 3;  
+    alert(`${numE} в кубе равняется ${cube}`);
+  }
+}
