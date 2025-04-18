bdce3a96bf89db797737153db491e1c5d5a8c386
Ariii17 <ariuna.uvannay@mail.ru>
2025-04-18 17:20:29 +0300

    сделала игру

--- scriptgame1.js
+++ scriptgame1.js
@@ -1,13 +1,12 @@
-document.querySelector('.gameButton').addEventListener('click', gameFirst);
-function gameFirst(guess) {
+document.querySelector('.gameButton').addEventListener('click', 
+function gameFirst() {
 let guess = prompt("Попробуйте угадать число");
-const randomNumber = 17; 
 if (guess < 0) {
 alert("Число не может быть отрицательным");
-} else if (randomNumber > 100) {
+} else if (guess > 100) {
 alert("Число не может быть больше 100");
-} else if (randomNumber = 17) {
+} else if (guess == 17) {
 alert ("Вы угадали!") ;
 } else 
 alert ("Вы ввели неправильное значениие");
-};
+});
