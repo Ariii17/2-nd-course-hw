a88727ecf346986ba71de575354a37b8d8471bf3
Ariii17 <ariuna.uvannay@mail.ru>
2025-04-18 17:04:32 +0300

    прробую делать задания и первую игру

---
+++ script6.js
@@ -0,0 +1,11 @@
+function numberLess(a, b) {
+    if (a <= b) {
+      return a;
+    } else {
+      return b;
+    }
+  }
+  
+  numberLess(8, 4);
+  
+  
---
+++ scriptgame1.js
@@ -0,0 +1,13 @@
+document.querySelector('.gameButton').addEventListener('click', gameFirst);
+function gameFirst(guess) {
+let guess = prompt("Попробуйте угадать число");
+const randomNumber = 17; 
+if (guess < 0) {
+alert("Число не может быть отрицательным");
+} else if (randomNumber > 100) {
+alert("Число не может быть больше 100");
+} else if (randomNumber = 17) {
+alert ("Вы угадали!") ;
+} else 
+alert ("Вы ввели неправильное значениие");
+});
