19b31e771fa54018e46fd66aec81a03e67fdc7f4
Ariii17 <ariuna.uvannay@mail.ru>
2025-04-20 11:38:17 +0300

    переделала недочеты, сделала 7 задание

--- script6.js
+++ script6.js
@@ -1,3 +1,4 @@
+/*1 task*/
 function numberLess(a, b) {
     if (a <= b) {
       return a;
@@ -6,14 +7,26 @@ function numberLess(a, b) {
     }
   }
 
+/*2 task*/
   function chet(a) {
     if (a % 2 == 0) {
-    alert("Число четное");
+    return 'Число четное';
     } else {
-    alert("Число нечетное");
+    return 'Число нечетное';
     }
+    }
+ /*3 task*/   
+    function outNumbSq(numC) {
+      const square = numC * numC;
+      console.log(square);
     }
     
+    function getSq(numD) {
+      const square = numD * numD;
+      console.log(square);
+      return numD;
+    }
+/*4 task*/
     let age = parseInt(prompt("Пожалуйста, введите ваш возраст"));
 
     if (isNaN(age)) {
@@ -21,55 +34,69 @@ function numberLess(a, b) {
     } else {
       function userAge() {
         if (age < 0) {
-          alert("Вы ввели неправильное значение (возраст не может быть отрицательным)");
+          alert('Вы ввели неправильное значение (возраст не может быть отрицательным)');
         } else if (age >= 13) {
-          alert("Добро пожаловать!");
+          alert( 'Добро пожаловать!');
         } else if (age <= 12) {
-          alert("Привет, друг!");
+          alert('Привет, друг!');
         }
       }
       userAge();
     }
-  
+ /*5 task*/ 
     function multNumb(a, b) {
       if (a === null || a === undefined || b === null || b === undefined) {
-        alert("Одно или оба значения являются null или undefined");
+        return 'Одно или оба значения являются null или undefined';
       }
     
       const numA = Number(a);
       const numB = Number(b);
     
       if (isNaN(numA) || isNaN(numB)) {
-        alert("Одно или оба значения не являются числом");
+        return 'Одно или оба значения не являются числом';
       } else {
-        alert(numA * numB);
+        return numA * numB;
       }
     }  
     
-function outNumbSq(numC) {
-  const square = numC * numC;
-  console.log(square);
-}
-
-function getSq(numD) {
-  const square = numD * numD;
-  console.log(square);
-  return numD;
-}
-
+  /*6 task*/
 function countCube() {
   const userInput = prompt("Пожалуйста, введите число:");
 
   if (userInput === null) {
-    return "Ввод отменен пользователем.";
+    return 'Ввод отменен пользователем.';
   }
 
   const numE = Number(userInput);
 
   if (isNaN(numE)) {
-    alert('Переданный параметр не является числом');
+    return'Переданный параметр не является числом';
   } else {
     const cube = numE ** 3;  
-    alert(`${numE} в кубе равняется ${cube}`);
+    return `${numE} в кубе равняется ${cube}`;
   }
 }
+
+/*7 task*/
+const circle1 = {
+  radius: 5,
+  getArea() {
+    return Math.PI * this.radius * this.radius;
+  },
+  getPerimeter() {
+    return 2 * Math.PI * this.radius;
+  }
+}
+const circle2 = {
+  radius: 7,
+  getArea() {
+    return Math.PI * this.radius * this.radius;
+  },
+  getPerimeter() {
+    return 2 * Math.PI * this.radius;
+  }
+}
+circle1.getArea();
+circle1.getPerimeter();
+circle2.getArea();
+circle2.getPerimeter();
